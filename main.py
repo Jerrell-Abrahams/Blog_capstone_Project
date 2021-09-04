02bfd8a879150cd2ec329f6cda94e8d640579978
Jerrell- Abrahams <jerrellabrahams50@gmail.com>
2021-09-04 15:31:42 +0200

    Changed Sql to Postgres

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from functools import wraps
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
+import os
 
 current_time = datetime.datetime.now().strftime("%B %d %Y")
 login_manager = LoginManager()
@@ -23,13 +23,13 @@ login_manager = LoginManager()
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager.init_app(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
